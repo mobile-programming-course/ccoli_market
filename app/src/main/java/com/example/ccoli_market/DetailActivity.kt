7d156f23a3afdf93176be56369a2735a0102dd34
jinhyuk Jeong <117005839+wjdwlsgurdla@users.noreply.github.com>
2023-11-26 20:16:36 +0900

    FEAT : 상세페이지 수정
    본인이 올린 글 확인 가능, 본인이 등록한 판매글이면 채팅버튼, 관심버튼 비활성화, 상세페이지 이동시 본인의 글이라는 ToastMessage 표시
    HomeFragment.kt / DetailActivity.kt 수정

--- app/src/main/java/com/example/ccoli_market/DetailActivity.kt
+++ app/src/main/java/com/example/ccoli_market/DetailActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ccoli_market.databinding.ActivityDetailBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 
 class DetailActivity : AppCompatActivity() {
 
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
     private lateinit var binding: ActivityDetailBinding
     private var isLiked = false
 
@@ -26,35 +33,53 @@ class DetailActivity : AppCompatActivity() {
         val content = intent.getStringExtra("content")
         val imageUrl = intent.getStringExtra("imageUrl")
 
-        Log.d("DetailActivity", "imageUrl:$imageUrl")
-
         Picasso.get().load(imageUrl).into(binding.detailImage)
 
-        //binding.detailImage.setImageResource(/* 이미지 리소스 ID 또는 URI */)
         binding.nickname.text = sellerId
         binding.detailTitle.text = title
         binding.detailContent.text = content
         binding.price.text = price
         binding.detailLikeIcon.setImageResource(if (isLiked) R.drawable.love_filled else R.drawable.love_empty)
 
-        binding.detailLikeIcon.setOnClickListener {
-            if (!isLiked) {
-                binding.detailLikeIcon.setImageResource(R.drawable.love_filled)
-                Snackbar.make(binding.constLayout, "관심 목록에 추가되었습니다.", Snackbar.LENGTH_SHORT).show()
-                isLiked = true
-            } else {
-                binding.detailLikeIcon.setImageResource(R.drawable.love_empty)
-                isLiked = false
+        if (auth.currentUser != null && auth.currentUser!!.uid == sellerId) {
+            // 판매자가 자신의 상품일 경우
+            showToast("내가 올린 상품입니다.")
+            disableButton(findViewById<Button>(R.id.messagebutton))
+        } else {
+            binding.detailLikeIcon.setOnClickListener {
+                // 상품에 대한 다른 동작 수행
+                if (!isLiked) {
+                    binding.detailLikeIcon.setImageResource(R.drawable.love_filled)
+                    showToast("관심 목록에 추가되었습니다.")
+                    isLiked = true
+                } else {
+                    binding.detailLikeIcon.setImageResource(R.drawable.love_empty)
+                    isLiked = false
+                }
             }
-        }
-        findViewById<Button>(R.id.messagebutton).setOnClickListener {
 
-            val chatIntent = Intent(this, ChattingRoomActivity::class.java)
-            startActivity(chatIntent)
+            findViewById<Button>(R.id.messagebutton).setOnClickListener {
+                // 판매자가 자신의 상품이 아닌 경우에만 동작
+                if (auth.currentUser != null && auth.currentUser!!.uid != sellerId) {
+                    val chatIntent = Intent(this, ChattingRoomActivity::class.java)
+                    startActivity(chatIntent)
+                } else {
+                    showToast("내가 올린 상품입니다.")
+                }
+            }
         }
         binding.backButton.setOnClickListener {
             onBackPressed()
         }
+    }
+
+    private fun disableButton(button: Button) {
+        button.isEnabled = false
+        button.isClickable = false
+        button.alpha = 0.5f
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
     private fun exit() {
         val likePosition = intent.getIntExtra("likePosition", 0)
