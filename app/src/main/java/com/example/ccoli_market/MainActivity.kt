ea1181d82cecf908d7d3026a1efe87861eae282c
Dasol <ekthf2013@naver.com>
2023-12-01 20:02:46 +0900

    FEAT: 자동로그인
    한번 로그인하면 로그아웃하기 전까지 로그인 안해도 됨 ㅎ

--- app/src/main/java/com/example/ccoli_market/MainActivity.kt
+++ app/src/main/java/com/example/ccoli_market/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.example.ccoli_market
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
-
-//1.로그인, 회원가입 디자인 다시 짜기 => 영서
-//2.게시물 수정할 수 있게 + 판매중판매완료 레이아웃 넣기 => 다솔
-//3.상세페이지 연결+ 필터링 =>진혁
-//4.채팅 레이아웃 => 시현
-
-
-
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check if user is signed in
+        val currentUser = Firebase.auth.currentUser
+        if (currentUser == null) {
+            // No user is signed in
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
 
         // 아래 3번에서 구현한 Fragment들을 가져옴
         val homeFragment = HomeFragment()
