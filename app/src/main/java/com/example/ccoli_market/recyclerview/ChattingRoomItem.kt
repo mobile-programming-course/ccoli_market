0f38f38d6cb4ca6d60c88ab19f9cb3d3e3a5d8ec
yeongseo <115892001+Cyeongseo@users.noreply.github.com>
2023-12-01 14:56:33 +0900

    FEAT: 채팅
    채팅..

--- app/src/main/java/com/example/ccoli_market/recyclerview/ChattingRoomItem.kt
+++ app/src/main/java/com/example/ccoli_market/recyclerview/ChattingRoomItem.kt
@@ -8,4 +8,9 @@ data class ChattingRoomItem(
     var chatUserName : String, //상대방 이름
     val lastChat : String, //마지막 채팅
     val userId: String // 상대방의 uId
+<<<<<<< Updated upstream
 ): Serializable
+=======
+
+)
+>>>>>>> Stashed changes
