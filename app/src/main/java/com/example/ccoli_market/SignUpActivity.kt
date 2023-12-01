4046890ab6c0937c50f077486dd0f6fd6cf659c2
yeongseo <115892001+Cyeongseo@users.noreply.github.com>
2023-12-01 14:12:13 +0900

    FEAT: 회원가입 데이터베이스 수정
    ## 작업 내용
    회원가입 내용 데이터베이스에 연동 users에 email,name,uid 순서로 저장

--- app/src/main/java/com/example/ccoli_market/SignUpActivity.kt
+++ app/src/main/java/com/example/ccoli_market/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.example.ccoli_market
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -10,87 +11,70 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+
+private lateinit var auth: FirebaseAuth
+lateinit var database: DatabaseReference
+
 class SignUpActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_page)
-        database = Firebase.database.reference
         auth = Firebase.auth
+        database = Firebase.database.reference
 
-        val checkBox = findViewById<CheckBox>(R.id.sign_up_checkBox)
-        val signUpButton = findViewById<Button>(R.id.sign_up_page_btn)
-        val backBtn = findViewById<ImageButton>(R.id.backBtn)
+        //ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
 
-        backBtnListener(backBtn)
+        val email = findViewById<EditText>(R.id.sign_up_email).text
+        val password = findViewById<EditText>(R.id.sign_up_password).text
+        val name = findViewById<EditText>(R.id.editTextName).text
+        val button = findViewById<Button>(R.id.sign_up_page_btn)
+        var profileCheck = false
 
-        // 초기값 설정
-        var isChecked = false
+        val intent = Intent(this, LoginActivity::class.java)
 
-        // 체크박스 상태 변경 리스너
-        checkBox.setOnCheckedChangeListener { _, isCheckedNow ->
-            isChecked = isCheckedNow
-            updateButtonListener(signUpButton, isChecked)
-        }
-
-        // 초기 버튼 리스너 설정
-        updateButtonListener(signUpButton, isChecked)
-    }
-
-    private fun backBtnListener(backBtn: ImageButton) {
-        backBtn.setOnClickListener {
-            onBackPressed()
-        }
-    }
-
-    private fun updateButtonListener(button: Button, isChecked: Boolean) {
         button.setOnClickListener {
-            if (isChecked) {
-                signUp()
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
+            if (email.isEmpty() && password.isEmpty() && name.isEmpty()) {
+                Toast.makeText(this, "아이디와 비밀번호를 제대로 입력해주세요.", Toast.LENGTH_SHORT).show()
+                Log.d("Email", "$email, $password")
             } else {
-                showToast(this, "정보제공 동의해주세요.")
+                auth.createUserWithEmailAndPassword(email.toString(), password.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val user = Firebase.auth.currentUser
+                            val userId = user?.uid
+                            val userIdSt = userId.toString()
+
+                            val friend = User(name.toString(),email.toString(),  userIdSt)
+                            database.child("users").child(userId.toString()).setValue(friend)
+
+                            Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                            Log.e(TAG, "$userId")
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "등록에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
             }
         }
     }
-
-    private fun signUp() {
-        val email = findViewById<EditText>(R.id.sign_up_email).text.toString()
-        val password = findViewById<EditText>(R.id.sign_up_password).text.toString()
-        val name=findViewById<EditText>(R.id.editTextName).text.toString()
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    showToast(this, "회원가입에 성공하였습니다.")
-//                    val user = auth.currentUser
-                    val uId = Firebase.auth.currentUser?.uid!! //현재 사용자의 uid
-                    addUser(name,email, uId)
-                }
-                else{
-                    showToast(this, "회원가입에 실패하였습니다.")
-                }
-            }
-        showToast(this, "회원가입에 성공하였습니다.")
-        val uId = Firebase.auth.currentUser?.uid!!
-        addUser(name,email, uId)
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
     }
-    private fun addUser(name:String,email: String,uId:String){
-        val user = User(name = name, email = email,uId=uId)
-        database.child("userInfo").child(uId).setValue(user)
-//        database.child("$uId").child("email").setValue(User(email))
-
-//        database.child("userInfo").child(uId).setValue(User(name,email,uId))
-
+    private fun reload() {
     }
-
-    private fun showToast(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 }
